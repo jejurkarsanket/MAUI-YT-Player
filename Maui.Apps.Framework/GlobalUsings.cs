1083075b62a9ac71e70249fe3fa6577c48cd11ac
Sanket Jejurkar <jejurkarsanket8@gmail.com>
2022-12-08 10:56:10 +0530

    1) Added List Extension and Boolean Converter. 2) Updtaed Info.plist for iOS. 3) Updated font to FiraSans.

--- Maui.Apps.Framework/GlobalUsings.cs
+++ Maui.Apps.Framework/GlobalUsings.cs
@@ -3,3 +3,5 @@ global using Maui.Apps.Framework.Exceptions;
 global using Maui.Apps.Framework.Extensions;
 global using System.Text.Json;
 global using System.Text;
+global using System.Collections.ObjectModel;
+global using System.Globalization;
