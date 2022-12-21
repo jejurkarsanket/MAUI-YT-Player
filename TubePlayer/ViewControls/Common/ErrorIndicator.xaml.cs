525e8d65165d5c64726d0bc700b867d8f2fbd342
Sanket Jejurkar <jejurkarsanket8@gmail.com>
2022-12-21 22:37:50 +0530

    Added code behind logic for Errorindicator Control.

--- TubePlayer/ViewControls/Common/ErrorIndicator.xaml.cs
+++ TubePlayer/ViewControls/Common/ErrorIndicator.xaml.cs
@@ -2,6 +2,41 @@ namespace TubePlayer.ViewControls.Common;
 
 public partial class ErrorIndicator : VerticalStackLayout
 {
+	public static BindableProperty IsErrorProperty = BindableProperty.Create(
+		"IsError",
+		typeof(bool),
+		typeof(ErrorIndicator),
+		false,
+		BindingMode.OneWay,
+		null,
+		SetIsError
+		);
+
+	public bool IsError
+	{ get => (bool)this.GetValue(IsErrorProperty) ;
+	  set => this.SetValue(IsErrorProperty, value); 
+	}
+
+	private static void SetIsError(BindableObject bindable, object oldValue, object newValue) =>
+		(bindable as ErrorIndicator).IsVisible = (bool)newValue;
+
+	public static readonly BindableProperty ErrorTextProperty = BindableProperty.Create(
+		"ErrorText",
+		typeof(string),
+		typeof(ErrorIndicator),
+		string.Empty,
+		BindingMode.OneWay,
+		null,
+		SetErrorText);
+
+	public string ErrorText
+	{
+		get => (string)this.GetValue(ErrorTextProperty);
+		set => this.SetValue(ErrorTextProperty, value);
+	}
+
+	private static void SetErrorText(BindableObject bindable, object oldValue, object newValue) =>
+		(bindable as ErrorIndicator).lblErrorText.Text = (string) newValue;
 	public ErrorIndicator()
 	{
 		InitializeComponent();
