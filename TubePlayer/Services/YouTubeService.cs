50a845540a47f9fb979e4484062c7a8baa1c189f
Sanket Jejurkar <jejurkarsanket8@gmail.com>
2022-12-22 23:44:14 +0530

    Added informational comment for YouTubeService.

--- TubePlayer/Services/YouTubeService.cs
+++ TubePlayer/Services/YouTubeService.cs
@@ -6,13 +6,16 @@ using TubePlayer.IServices;
 
 namespace TubePlayer.Services
 {
+    // YouTubeService Inherits ResrServiceBase class from Maui.Apps.Framework and Implements IApiService
     public class YouTubeService : RestServiceBase, IApiService    
     {
         public YouTubeService(IConnectivity connectivity, IBarrel cacheBarrel): base(connectivity, cacheBarrel)
         {
+            //Set API Service URL
             SetBaseURL(Constants.ApiServiceURL);
         }
-
+        
+        //Method for Searching Videoes 
         public async Task<VideoSearchResult> SearchVideoes(string searchQuery, string nextPageToken = "")
         {
             var resourceUri = $"search?part=snippet&maxResults=10&type=video&key={Constants.ApiKey}&q={WebUtility.UrlEncode(searchQuery)}"
