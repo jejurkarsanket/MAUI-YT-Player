702a2d00733737510142b8386147a32f9eae8008
Sanket Jejurkar <jejurkarsanket8@gmail.com>
2022-12-22 23:46:37 +0530

    Added comment for IApiService

--- TubePlayer/IServices/IApiService.cs
+++ TubePlayer/IServices/IApiService.cs
@@ -3,6 +3,7 @@ namespace TubePlayer.IServices
 {
     public interface IApiService
     {
+        //Method for Searching Videoes
         Task<VideoSearchResult> SearchVideoes(string searchQuery, string nextPageToken = "");
     }
 }
