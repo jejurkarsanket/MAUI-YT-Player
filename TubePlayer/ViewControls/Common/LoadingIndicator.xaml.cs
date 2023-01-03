3daaf309c7efd39dffc6eba017d83d056c8a5977
Sanket Jejurkar <jejurkarsanket8@gmail.com>
2023-01-03 23:52:45 +0530

    Added code behind for Loading Indicator

--- TubePlayer/ViewControls/Common/LoadingIndicator.xaml.cs
+++ TubePlayer/ViewControls/Common/LoadingIndicator.xaml.cs
@@ -5,6 +5,31 @@ namespace TubePlayer.ViewControls.Common;
 public partial class LoadingIndicator : VerticalStackLayout
 {
 	// Control for showing loading screen.
+	//Bindable Properties
+
+	public static readonly BindableProperty IsBusyProperty = BindableProperty.Create(
+		"IsBusy",
+		typeof(bool),
+		typeof(LoadingIndicator),
+		false,
+		BindingMode.OneWay,
+		null,
+		SetIsBusy);
+
+	public bool IsBusy
+	{
+		get => (bool)this.GetValue(IsBusyProperty);
+		set => this.SetValue(IsBusyProperty, value);
+	}
+
+	private static void SetIsBusy(BindableObject bindable, object oldvalue, object newValue)
+	{
+		LoadingIndicator control = bindable as LoadingIndicator;
+
+		control.IsVisible = (bool)newValue;
+		control.actIndicator.IsRunning = (bool)newValue;
+	}
+	
 	public LoadingIndicator()
 	{
 		InitializeComponent();
