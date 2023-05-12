f381c51068d98b8856bdd6ed43eccc18d25a6863
Sanket Jejurkar <jejurkarsanket8@gmail.com>
2023-05-12 10:42:21 +0530

    Added space into ServiceHelper for test

--- TubePlayer/Helpers/ServiceHelper.cs
+++ TubePlayer/Helpers/ServiceHelper.cs
@@ -5,6 +5,7 @@
         //Select services based on Platfrom Type
         public static TService GetService<TService>() =>
             Current.GetService<TService>();
+
         public static IServiceProvider Current =>
 #if WINDOWS10_0_17763_0_OR_GREATER
                     MauiWinUIApplication.Current.Services;
